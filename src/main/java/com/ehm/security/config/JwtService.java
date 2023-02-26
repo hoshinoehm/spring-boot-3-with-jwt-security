47e0e8dde72729d9bd6ba7a03087edb1eacaf404
hoshinoehm <hoshinoehm@gmail.com>
2023-02-26 17:00:49 -0300

    Iniciando o projeto spring com JWT

---
+++ src/main/java/com/ehm/security/config/JwtService.java
@@ -0,0 +1,23 @@
+package com.ehm.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+}
---
+++ src/main/java/com/ehm/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.ehm.security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
